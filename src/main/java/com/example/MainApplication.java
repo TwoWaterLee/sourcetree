a437dd185836cca5073a6a3ddcb76f4e098838d0
liershui <liershui@webull.com>
2021-05-10 19:47:03 +0800

    Second commit

---
+++ src/main/java/com/example/MainApplication.java
@@ -0,0 +1,14 @@
+package com.example;
+
+/**
+ * TODO
+ *
+ * @author liershui
+ * @date 2021/5/10 11:45 上午
+ */
+public class MainApplication {
+
+    public static void main(String[] args) {
+        System.out.println("Hello");
+    }
+}
